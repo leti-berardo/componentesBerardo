6f27fdddda4a5d38aaad33e1456946f2ffffc9a1
letiberardo <leticiaberardo@gmail.com>
2022-05-09 22:29:45 -0300

    componentesBerardo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBar } from './components/nav-bar/nav-bar.component';
+import { MenuBar } from './components/menu-bar/menu-bar.component';
+import { MainContent } from './components/main-content/main-content.component';
+import { NuevoComponenteComponent } from './components/nuevo-componente/nuevo-componente.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavBar, MenuBar, MainContent, NuevoComponenteComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NavBar, MenuBar, MainContent],
 })
-export class AppModule { }
+
+export class AppModule {}
---
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.scss']
+})
+export class MenuBar {
+  title = 'menu-bar';
+
+
+}
